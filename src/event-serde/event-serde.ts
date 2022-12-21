d032031ab41ca991e9c018b9cc1059305f81c315
Alec Lombardo <alec.lombardo@gmail.com>
2022-12-21 13:00:44 -0500

    feat: add basic json serde

---
+++ src/event-serde/event-serde.ts
@@ -0,0 +1,6 @@
+import { DendriteEvent } from '../event/dendrite-event'
+
+export abstract class EventSerde {
+  abstract serialize<T extends DendriteEvent>(obj: T): string
+  abstract deserialize<T extends DendriteEvent>(raw: string, constructor: T): T
+}
---
+++ src/event-serde/json-serde.spec.ts
@@ -0,0 +1,46 @@
+import { DendriteEvent } from '../event/dendrite-event'
+import { JSONSerde } from './json-serde'
+
+describe(JSONSerde.name, () => {
+  class MockEvent extends DendriteEvent {
+    constructor(
+      id: string,
+      public name: string,
+      public time: Date,
+      public bool: boolean
+    ) {
+      super(id)
+    }
+  }
+
+  describe('deserialize', () => {
+    it('should convert from a JSON string', () => {
+      const mockObj = new MockEvent(
+        '1',
+        'name',
+        new Date(2022, 1, 2, 3, 4, 5, 6),
+        false
+      )
+      const result = new JSONSerde().deserialize(
+        '{"aggregateId":"1","name":"name","time":"2022-02-02T08:04:05.006Z","bool":false}',
+        MockEvent.prototype
+      )
+      expect(result).toEqual(mockObj)
+    })
+  })
+
+  describe('serialize', () => {
+    it('should produce a JSON string', () => {
+      const mockObj = new MockEvent(
+        '1',
+        'name',
+        new Date(2022, 1, 2, 3, 4, 5, 6),
+        false
+      )
+      const result = new JSONSerde().serialize(mockObj)
+      expect(result).toEqual(
+        '{"aggregateId":"1","name":"name","time":"2022-02-02T08:04:05.006Z","bool":false}'
+      )
+    })
+  })
+})
